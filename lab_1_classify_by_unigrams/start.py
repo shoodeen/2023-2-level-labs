5b351898cf2ff6c39d9b1437c720833fa4290009
artyomtugaryov <artyomtugaryov@users.noreply.github.com>
2023-10-11 11:01:02 +0300

    checkout labs from the origin repository

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,7 +1,6 @@
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams.main import create_language_profile
 
 from lab_1_classify_by_unigrams.main import (collect_profiles, create_language_profile,
                                              detect_language_advanced, print_report)
